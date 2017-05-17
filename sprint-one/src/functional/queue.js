faa293a22e741989e621ff7b3f02fbed8d32be55
artliou <artliou@thatoneart.local>
2017-05-17 14:30:46 -0700

    yay

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,25 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var length = 0;
   // Implement the methods below
-
+  //Add a string to the back of the queue
   someInstance.enqueue = function(value) {
+    this.elements.push(value);
+    length++;
   };
 
+  //Remove and return the string at the front of the queue
   someInstance.dequeue = function() {
+    var ripped = someInstance[0];
+    someInstance.shift();
+    return ripped;
+    length--;
+    return this.elements.shift();
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[storage.size + 1] = value;
+    size++;
+    return size;
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function() {      
+    var out = someInstance[someInstance.size - 1];
+    //this.splice(-1);
+    size--;
+    if (size <= 0) {
+      size = 0;
+    }
+    return out;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
